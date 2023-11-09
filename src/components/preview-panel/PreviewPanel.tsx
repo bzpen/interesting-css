8211e118c6b8cc3246ff07390b3c9da4396824c3
kaito_kuroba <3118659412@qq.com>
2023-11-09 09:40:04 +0800

    style: a 标签跳转新链接

--- src/components/preview-panel/PreviewPanel.tsx
+++ src/components/preview-panel/PreviewPanel.tsx
@@ -7,7 +7,9 @@ const PreviewPanel = () => {
             <div className={styles.title}>Solid Shadow Word</div>
             <div className={styles.inspiration_origin}>
                 <span>Source of inspiration:</span>
-                <a href='https://github.com/chokcoco/iCSS/issues/219'>https://github.com/chokcoco/iCSS/issues/219</a>
+                <a href='https://github.com/chokcoco/iCSS/issues/219' target='_blank'>
+                    https://github.com/chokcoco/iCSS/issues/219
+                </a>
             </div>
             <div className={styles.preview_box}>
                 <SolidShadowWord word={'Solid Shadow Word'} />
