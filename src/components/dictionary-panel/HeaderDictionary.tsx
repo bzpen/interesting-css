4d1ae2f5b2361be5e2152e010678061c23dc541a
kaito_kuroba <3118659412@qq.com>
2023-12-12 20:34:19 +0800

    feat: 头部样式调整

--- src/components/dictionary-panel/HeaderDictionary.tsx
+++ src/components/dictionary-panel/HeaderDictionary.tsx
@@ -1,7 +1,21 @@
+import GithubIcon from '../../assets/icon/github.svg?react';
+import MoonIcon from '../../assets/icon/moon.svg?react';
+import SanIcon from '../../assets/icon/san.svg?react';
 import styles from '../../style/css/HeaderDictionary.module.less';
 
 const HeaderDictionary = () => {
-    return <div className={styles.box}>Header</div>;
+    return (
+        <div className={styles.box}>
+            <div className={styles.title}>Interesting CSS</div>
+            <div className={styles.icon_list}>
+                <a href='https://github.com/bzpen/interesting-css' target='_blank'>
+                    <GithubIcon className={styles.btu_icon_github} />
+                </a>
+                <MoonIcon className={styles.btu_icon_moon} />
+                <SanIcon className={styles.btu_icon_san} />
+            </div>
+        </div>
+    );
 };
 
 export default HeaderDictionary;
